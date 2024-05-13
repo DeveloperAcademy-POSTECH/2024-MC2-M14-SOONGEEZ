a2c85eb0f7ae1562cb261f86b47dbf60e78c534b
ezzkimm <dmswjd1515@gmail.com>
2024-05-13 11:05:02 +0900

    init project

---
+++ SOONGEEZ/SOONGEEZ/SOONGEEZApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOONGEEZApp.swift
+//  SOONGEEZ
+//
+//  Created by 김은정 on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SOONGEEZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
