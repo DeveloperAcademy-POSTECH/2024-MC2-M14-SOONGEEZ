ccec724e61af54235b2ba0b96fca58420f2f444d
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-05-13 11:43:31 +0900

    ColorExtension 추가

--- SOONGEEZ/SOONGEEZ/Extension/ColorExtension.swift
+++ SOONGEEZ/SOONGEEZ/Extension/ColorExtension.swift
@@ -1,8 +1,21 @@
-//
-//  ColorExtension.swift
-//  SOONGEEZ
-//
-//  Created by 조세연 on 5/13/24.
-//
+import SwiftUI
 
-import Foundation
+
+extension Color{
+    static let customGreen = Color(hex: "7B926C") //이런 식으로 색상 넣기
+}
+
+extension Color {
+  init(hex: String) {
+    let scanner = Scanner(string: hex)
+    _ = scanner.scanString("#")
+    
+    var rgb: UInt64 = 0
+    scanner.scanHexInt64(&rgb)
+    
+    let r = Double((rgb >> 16) & 0xFF) / 255.0
+    let g = Double((rgb >>  8) & 0xFF) / 255.0
+    let b = Double((rgb >>  0) & 0xFF) / 255.0
+    self.init(red: r, green: g, blue: b)
+  }
+}
