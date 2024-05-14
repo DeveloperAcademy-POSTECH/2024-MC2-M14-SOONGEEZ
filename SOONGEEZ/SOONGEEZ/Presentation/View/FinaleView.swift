a93aea5ad8dc2c0d65c40eec93279479e84a2939
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-05-14 10:08:37 +0900

    [FEAT/#1] 모든 뷰 만들기

---
+++ SOONGEEZ/SOONGEEZ/Presentation/View/FinaleView.swift
@@ -0,0 +1,18 @@
+//
+//  FinaleView.swift
+//  SOONGEEZ
+//
+//  Created by 조세연 on 5/14/24.
+//
+
+import SwiftUI
+
+struct FinaleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinaleView()
+}
---
+++ SOONGEEZ/SOONGEEZ/Presentation/View/FinishView.swift
@@ -0,0 +1,18 @@
+//
+//  FinishView.swift
+//  SOONGEEZ
+//
+//  Created by 조세연 on 5/14/24.
+//
+
+import SwiftUI
+
+struct FinishView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinishView()
+}
