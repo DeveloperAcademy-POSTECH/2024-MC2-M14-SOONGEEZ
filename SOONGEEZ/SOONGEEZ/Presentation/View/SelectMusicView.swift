8ba73058f320e0f81d01447bd7f64e5cace17d5a
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-05-15 17:53:42 +0900

    [FEAT/#3] 검색 창 구현

--- SOONGEEZ/SOONGEEZ/Presentation/View/SelectMusicView.swift
+++ SOONGEEZ/SOONGEEZ/Presentation/View/SelectMusicView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct SelectMusicView: View {
+    
+    @State private var searchText = ""
+    
+    
     var body: some View {
-        VStack{
+        VStack(alignment: .leading,
+               spacing: 8){
+            Text("피날레 곡")
+                .padding(.leading, 20)
+                .fontWeight(.bold)
+                .font(.system(size: 24))
+            Text("오늘의 피날레를 장식할 곡을 선택해 주세요.")
+                .padding(.leading, 20)
+                .font(.system(size: 17))
+            
+            SearchBar(text: $searchText)
+                .padding(EdgeInsets(top: 18, leading: 0, bottom: 16, trailing: 0))
+            
+            
         }
+        
+        
+       
     }
 }
 
