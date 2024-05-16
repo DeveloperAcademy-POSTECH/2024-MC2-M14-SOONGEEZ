855929056dd4a4cbad060221f880140e0e32107b
Yunji Lee <yundlee15@gmail.com>
2024-05-16 18:18:23 +0900

    [FEAT/#4] 뮤직 뷰 UI 구현 (백그라운드 이미지 추가)

--- SOONGEEZ/SOONGEEZ/Presentation/View/MusicView.swift
+++ SOONGEEZ/SOONGEEZ/Presentation/View/MusicView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct MusicView: View {
     var body: some View {
         ZStack{
-            
-            //Image("") //백그라운드 이미지 들어갈 것
+            Image("img_musicViewBackground") //백그라운드 이미지 들어갈 것
+                .ignoresSafeArea()
+                
             
             VStack(spacing:0){
                 
@@ -22,11 +23,9 @@ struct MusicView: View {
                 CurrentPlayingSong
                 
                 HStack{
-                    StopButton
-                    
-                 //   SkipButton
+                    PauseButton
                 }
-                .padding([.horizontal],20)
+                
                 
                 Spacer()
             }
@@ -37,11 +36,11 @@ struct MusicView: View {
 
 var TopLogo: some View {
     HStack{
-        Image("textLogo")//텍스트 로고
+        Image("img_logo")//텍스트 로고
             .resizable()
             .scaledToFit()
             .frame(width:69, height: 21)
-            .padding([.top], 15)
+            .padding([.top], 62)
             .padding([.bottom], 24)
             .padding([.horizontal], 20)
         Spacer()
@@ -70,29 +69,35 @@ var CurrentOrder: some View {
 var CurrentPlayingSong: some View {
     VStack(spacing:0){
         VStack(spacing:0){
-//         //   Image("")//
-//                .resizable()
-//                .scaledToFill()
-//                .frame(width:305, height:305)
-//                .clipShape(RoundedRectangle(cornerRadius: 10))
-//                .clipped()
-//                .padding([.bottom],20)
+            Image("img_recordAudio")
+                .resizable()
+                .scaledToFill()
+                .frame(width:305, height:305)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .clipped()
+                .padding([.bottom],20)
             
-            VStack(spacing:12){//songinfo
-                HStack{
-                    Text("노래제목")//data: 노래제목
-                        .font(.system(size: 24, weight:.semibold))
-                    Spacer()
+            HStack(alignment:.center,spacing: 5) {
+                VStack(spacing:12){//songinfo
+                    HStack{
+                        Text("노래제목")//data: 노래제목
+                            .font(.system(size: 24, weight:.semibold))
+                        Spacer()
+                    }
+                    HStack{
+                        Text("가수")//data: 가수
+                            .font(.system( size: 20))
+                        Spacer()
+                    }
                 }
-                HStack{
-                    Text("가수")//data: 가수
-                        .font(.system( size: 20))
-                    Spacer()
-                }
+                SkipButton
             }.padding([.bottom], 20)
             
             VStack(spacing:0){//songLength
-                //재생바
+               RoundedRectangle(cornerRadius: 5) //재생바
+                    .foregroundColor(.customLightGray)
+                    .frame(height:8)
+                    .padding([.bottom],4)
                 HStack{
                     Text("0:30")//data: 현재 재생시간
                         .font(.system( size: 16))
@@ -105,37 +110,47 @@ var CurrentPlayingSong: some View {
     }
     .padding(24)
     .background{
-        RoundedRectangle(cornerRadius: 20)
-            .fill(Color.blue)
+        RoundedRectangle(cornerRadius: 16)
+            .fill(.ultraThickMaterial)
             .strokeBorder(Color.black, lineWidth:1)
+         
+            
     }
     .padding([.horizontal],20)
     .padding([.bottom],16)
-    
 }
 
-var StopButton: some View{
-    
+var SkipButton: some View {
     Button(action:{
         print("이 곡 넘기기")
+    }, label: {
+        Image(systemName: "forward.end.fill")
+            .font(.system( size: 20))
+            .foregroundColor(.customGray)
+            .frame(width:40, height:40)
+    }
+    )
+}
+
+var PauseButton: some View{
+    Button(action:{
+        print("일시정지 하고 플레이리스트 갱신")
     }, label: {
         HStack(spacing:4){
             Image(systemName: "pause.fill")
             Text("일시정지")
-        }.foregroundColor(.black)
+        }.foregroundColor(.primaryPurple)
     }
     )
     .padding([.vertical],14)
-    .padding([.horizontal],20)
+    .padding([.horizontal],17.5)
     .background{
-        RoundedRectangle(cornerRadius: 10)
-            .fill(Color.orange)
-            .strokeBorder(Color.black, lineWidth:1)
+        RoundedRectangle(cornerRadius: 12)
+            .fill(Color.white)
     }
 }
 
 
-//여기부터 추가
 
 
 
