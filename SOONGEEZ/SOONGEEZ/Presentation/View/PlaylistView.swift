b6eab1391368825069134f3580f241180993941a
ezzkimm <dmswjd1515@gmail.com>
2024-05-17 10:01:37 +0900

    [FEAT/#7] Alert 구현

--- SOONGEEZ/SOONGEEZ/Presentation/View/PlaylistView.swift
+++ SOONGEEZ/SOONGEEZ/Presentation/View/PlaylistView.swift
@@ -20,14 +20,23 @@ struct Music: Identifiable {
 struct PlaylistView: View {
     @State var MusicList: [Music] = [
         Music(title: "Cookie", artist: "NeaJeans", lenght: "3:13"),
-        Music(title: "Starlight", artist: "Muse", lenght: "4:12")
-
-        
+        Music(title: "Starlight", artist: "Muse", lenght: "4:12"),
+        Music(title: "Cookie", artist: "NeaJeans", lenght: "3:13"),
+        Music(title: "Starlight", artist: "Muse", lenght: "4:12"),
+        Music(title: "Cookie", artist: "NeaJeans", lenght: "3:13"),
+        Music(title: "Starlight", artist: "Muse", lenght: "4:12"),
+        Music(title: "Cookie", artist: "NeaJeans", lenght: "3:13"),
+        Music(title: "Starlight", artist: "Muse", lenght: "4:12"),
+        Music(title: "Cookie", artist: "NeaJeans", lenght: "3:13"),
+        Music(title: "Starlight", artist: "Muse", lenght: "4:12"),
     ]
+    @State private var showingAlert = false
+    
+    
     
     var body: some View {
         
-        VStack(alignment: .leading){
+        VStack(alignment: .leading, spacing: 20){
             Text("5월 12일의 플레이리스트")
                 .font(.system(size: 24, weight: .semibold))
                 .padding(.leading, 20)
@@ -37,54 +46,78 @@ struct PlaylistView: View {
                 Spacer()
                 Text("13분 25초")
                     .padding(.trailing, 20)
+                    .font(.footnote)
+                    .foregroundColor(.gray)
             }
             
-            
-            List{
-                ForEach(MusicList, id: \.id) { item in
-                    HStack{
-                        Rectangle()
-                            .cornerRadius(11)
-                            .frame(width: 44, height: 44)
-                        
-                        
-                        
-                        VStack(alignment: .leading)
-                        {
-                            Text(item.title)
-                            Text(item.artist)
-                                .font(.subheadline)
+            ZStack(alignment: .bottom){
+                
+                List{
+                    ForEach(MusicList, id: \.id) { item in
+                        HStack{
+                            Rectangle()
+                                .cornerRadius(11)
+                                .frame(width: 44, height: 44)
+                            
+                            
+                            
+                            VStack(alignment: .leading)
+                            {
+                                Text(item.title)
+                                Text(item.artist)
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                            Spacer()
+                            Text(item.lenght)
                                 .foregroundColor(.gray)
+                                .font(.caption)
+                            
                         }
-                        Spacer()
-                        Text(item.lenght)
-                            .foregroundColor(.gray)
                         
                     }
-                    
+                    .onDelete(perform: delete)
                 }
-                .onDelete(perform: delete)
+                .listStyle(.inset)
+                .frame(maxHeight: 560)
+                
+                VStack{
+                    Spacer()
+                    HStack(alignment: .bottom){
+                        Spacer()
+                        Text("플레이리스트 종료하기")
+                            .font(Font.custom("Inter", size: 14))
+                            .underline()
+                            .foregroundColor(.gray)
+                            .onTapGesture {
+                                showingAlert = true
+                            }
+                        Spacer()
+                    }
+                }
+                .frame(width: 393, height: 87)
+                .background(
+                    LinearGradient(
+                        stops: [
+                            Gradient.Stop(color: .white.opacity(0), location: 0.00),
+                            Gradient.Stop(color: .white, location: 1.00),
+                        ],
+                        startPoint: UnitPoint(x: 0.5, y: 0.18),
+                        endPoint: UnitPoint(x: 0.5, y: 0.5)
+                    ))
+                
             }
-            .listStyle(.inset)
-            .frame(maxHeight: 540)
-            
-            
-            HStack{
-                Spacer()
-                Text("플레이리스트 종료하기")
-                    .font(Font.custom("Inter", size: 14))
-                    .underline()
-                    .foregroundColor(.gray)
-                Spacer()
-            }
-            
         }
         
-//        .padding(20)
+        .alert("플레이리스트 종료", isPresented: $showingAlert) {
+            Button("종료하기", role: .destructive) {}
+            Button("취소", role: .cancel) {}
+            
+        } message: {
+            Text("지금 재생하고 있는 플레이리스트를 종료하시겠습니까?")
+        }
         
-        
-        
-        
+
         
     }
     func delete(at offsets: IndexSet) {
@@ -94,6 +127,7 @@ struct PlaylistView: View {
     }
     
 }
+
 
 
 
