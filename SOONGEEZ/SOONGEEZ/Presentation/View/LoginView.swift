6758b226a2de1de8fa70fb1abdc8eeabd30527e4
Anna_Hyerim <hyerim46@swu.ac.kr>
2024-05-17 09:25:18 +0900

    [FEAT/#5] 로그인 뷰 UI 구현 (스플래시뷰랑 로그인뷰 합쳐짐!!)

--- SOONGEEZ/SOONGEEZ/Presentation/View/LoginView.swift
+++ SOONGEEZ/SOONGEEZ/Presentation/View/LoginView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack() {
+            Image("backgroundOfSplash")
+                .scaledToFill()
+                .ignoresSafeArea()
+            VStack() {
+                Spacer()
+                Button(action: {
+                    print("")
+                }, label: {
+                    Image("iconOfYoutube")
+                        . frame(width: 30,height: 30)
+                    Text("Youtube Music 연결하기")
+                        .foregroundColor(.primary)
+                })
+                .frame(maxWidth: .infinity)
+                .fontWeight(.regular)
+                .padding(10)
+                .background(
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(Color.white)
+                        .strokeBorder(lineWidth: 1)
+                )
+                .padding(.horizontal, 20)
+                .padding(.bottom, 80)
+            }
+        }
     }
 }
 
