664f3962bcf531df60c658f895af86d6b3a18566
seeeRini <serin2669@naver.com>
2024-05-20 09:52:56 +0900

    [FEAT/#6] test 뷰 삭제

--- SOONGEEZ/SOONGEEZ/Presentation/View/SplashView.swift
+++ SOONGEEZ/SOONGEEZ/Presentation/View/SplashView.swift
@@ -1,18 +1,1 @@
-//
-//  SplashView.swift
-//  SOONGEEZ
-//
-//  Created by 조세연 on 5/14/24.
-//
 
-import SwiftUI
-
-struct SplashView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
-}
-
-#Preview {
-    SplashView()
-}
