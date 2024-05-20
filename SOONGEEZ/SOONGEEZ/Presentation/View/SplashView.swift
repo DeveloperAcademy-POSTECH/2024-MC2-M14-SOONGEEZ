2a368fae4bd544e5773e58ce60ce261b84d3065c
Yunji Lee <yundlee15@gmail.com>
2024-05-20 12:51:38 +0900

    [FEAT/#4] 컨플릭 해결

--- SOONGEEZ/SOONGEEZ/Presentation/View/SplashView.swift
+++ SOONGEEZ/SOONGEEZ/Presentation/View/SplashView.swift
@@ -1,15 +1,9 @@
-//
-//  SplashView.swift
-//  SOONGEEZ
-//
-//  Created by 조세연 on 5/14/24.
-//
 
 import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("로그인뷰랑 스플래시뷰 합쳐짐")
     }
 }
 
