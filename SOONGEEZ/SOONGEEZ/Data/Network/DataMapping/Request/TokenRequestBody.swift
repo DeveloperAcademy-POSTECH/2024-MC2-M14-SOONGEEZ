472abdf55eb83e0ba81f6ef230a185bdb6ce28b8
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-05-21 18:28:14 +0900

    [FEAT/#15] 구글 ouath api 최종 구현 완료

---
+++ SOONGEEZ/SOONGEEZ/Data/Network/DataMapping/Request/TokenRequestBody.swift
@@ -0,0 +1,8 @@
+//
+//  TokenRequestBody.swift
+//  SOONGEEZ
+//
+//  Created by 조세연 on 5/21/24.
+//
+
+import Foundation
---
+++ SOONGEEZ/SOONGEEZ/Data/Network/NetwokManager/Login/TokenService.swift
@@ -0,0 +1,8 @@
+//
+//  TokenService.swift
+//  SOONGEEZ
+//
+//  Created by 조세연 on 5/21/24.
+//
+
+import Foundation
